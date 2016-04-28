39063a58d489f21487e0f9662d865b91db946f28
mohammedzee1000 <mohammed.zee1000@gmail.com>
2016-04-28 20:26:57 +0530

    Added ability to specify action.

--- letsencrypt/centos7/gencerts.py
+++ letsencrypt/centos7/gencerts.py
@@ -6,10 +6,12 @@ from subprocess import call;
 
 cert_urls = os.environ['CERT_URL_LIST']
 cert_meth = os.environ['CERT_METH']
+cert_action = os.environ['CERT_ACTION']
+
 
 urls = cert_urls.split(";");
 
-CMD = ["letsencrypt", "certonly", cert_meth, "--agree-tos", "--register-unsafely-without-email", "--dry-run"]
+CMD = ["letsencrypt", cert_action, cert_meth, "--agree-tos", "--register-unsafely-without-email", "--dry-run"]
 
 for url in urls:
     CMD.append("-d")
